f59a800777b81392f4b34c835fd52b0de6824ec0
Terry Lu Zijian <terryluzijian@gmail.com>
2017-10-09 19:07:11 +0800

    Crawler Code Change
    Added more details on crawler

--- Web Crawler/crawler/crawler/spiders/school_spider.py
+++ Web Crawler/crawler/crawler/spiders/school_spider.py
@@ -2,7 +2,6 @@ import re
 import scrapy
 from crawler.items import SchoolWebPageItem
 from datetime import datetime
-from scrapy.exceptions import NotSupported
 from scrapy.loader import ItemLoader
 from tld import get_tld
 
@@ -13,16 +12,38 @@ class SchoolWebsiteContentSpider(scrapy.Spider):
     allowed_domains = []
 
     # Customized filtering params
-    FILTERED_TAG_NAME = ['script', 'style', 'title']
-    FILTERED_WORD_NAME = ['library', 'wiki', 'login', 'new', 'lib.']
+    FILTERED_TAG_NAME = ('script', 'style', 'title')
+    FILTERED_WORD_NAME = ('library', 'wiki', 'login', 'new', 'lib.')
+    FILTERED_FILE_SUFFIX = ('js', 'css', 'img', 'png')
+
+    # Constant
+    STRING_LENGTH_LIMIT = 4
+    EMAIL_UPPER_BOUND = 3
+    EMAIL_LOWER_BOUND = 1
+
+    # Specific word list
+    PROFILE_SPECIFIC_WORD = {
+        'PERSONAL': ('biography', 'contact', 'email', 'phone number', 'tel', 'office'),
+        'POST_FULL': ('professor', 'lecturer', 'associate professor', 'assistant professor', 'doctor'),
+        'POST_ABBR': ('prof', 'a/p', 'assoc prof', 'dr'),
+        'RESEARCH': ('research interests', 'specialized areas', 'research areas'
+                     'publications', 'publication', 'selected publications')
+    }
+    RANK = sorted({
+        'RESEARCH': 1,
+        'EMAIL': 2,
+        'POST_ABBR': 3,
+        'POST_FULL': 4,
+        'PERSONAL': 5,
+    }.items(), key=lambda x: x[1])
 
     # Fixed XPath
     TITLE_XPATH = '//title/text()'
-    STRING_LENGTH_LIMIT = 4
     TEXT_XPATH = '//*[not(%s)]/text()[normalize-space(.)][string-length() >= %d]' % \
                  ('|'.join(list(map(lambda x: 'self::%s' % x, FILTERED_TAG_NAME))), STRING_LENGTH_LIMIT)
     LINK_XPATH = '//*/attribute::src|//*/attribute::href'
     PDF_XPATH = '//*/attribute::src[contains(., \'.pdf\')]|//*/attribute::href[contains(., \'.pdf\')]'
+    EMAIL_XPATH = '//*/attribute::*[contains(., \'mail\')]'
 
     # Regex
     EMAIL_REGEX = r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)'
@@ -52,7 +73,7 @@ class SchoolWebsiteContentSpider(scrapy.Spider):
             return False
 
         # if the url is neither a html document nor a pdf file (CV)
-        if '<html>' not in response.body:
+        if (url.split('.')[-1] in self.FILTERED_FILE_SUFFIX) | ('<html>' not in str(response.body)):
             return False
 
         # if the url contains words in the filtered word list
@@ -61,6 +82,73 @@ class SchoolWebsiteContentSpider(scrapy.Spider):
                 return False
 
         return True
+
+    def determine_personal_profile_component(self, **kwargs):
+        # Help function to locate certain keywords
+        def iterate_and_find(text_content_list, word_list, abbreviated=False):
+            for element in word_list:
+                if not abbreviated:
+                    if len(list(filter(lambda x: x == element, text_content_list))) > 0:
+                        return True
+                else:
+                    if len(list(filter(lambda x: x in element, text_content_list))) > 0:
+                        return True
+            return False
+
+        # Help function for logic operation
+        def compare_logic_pair(cond_dict, rank):
+            init_cond = False
+            for item in rank:
+                init_cond = init_cond | cond_dict[item[0]]
+                if init_cond:
+                    return init_cond
+
+        # Parse kwargs
+        try:
+            email_list = kwargs['email']
+            # Get the lowercase for later comparison
+            text_list = list(map(lambda x: x.lower(), kwargs['text']))
+        except KeyError:
+            return False
+
+        # Enumerate the conditions
+        condition_dict = {}
+        email_condition = (len(email_list) >= self.EMAIL_LOWER_BOUND) & (len(email_list) <= self.EMAIL_UPPER_BOUND)
+        condition_dict['EMAIL'] = email_condition
+        for key, values in self.PROFILE_SPECIFIC_WORD.items():
+            condition_dict[key] = iterate_and_find(text_content_list=text_list,
+                                                   word_list=self.PROFILE_SPECIFIC_WORD[key],
+                                                   abbreviated=True if 'abbr' in key.lower() else False)
+        return compare_logic_pair(condition_dict, self.RANK)
+
+    def fetch_email_list(self, response):
+        # Get email address from html body
+        email_match = re.findall(self.EMAIL_REGEX, str(response.body))
+        email_list = set(email_match)
+
+        # Get email address from attribute value
+        email_xpath_extract = ' '.join(response.xpath('//*/attribute::*[contains(., \'mail\')]').extract())
+        email_xpath_match = re.findall(self.EMAIL_REGEX, email_xpath_extract)
+
+        # Strip email found in the xpath
+        for email in email_xpath_match:
+            email_list.add(email)
+            email_xpath_extract = email_xpath_extract.replace(email, '')
+
+        # Find match again if the email address is split in the attribute value
+        email_xpath_match_again = re.findall(r'\'([a-zA-Z0-9_.+-]+)\'', email_xpath_extract)
+        if len(email_xpath_match_again) >= 2:
+            if re.match(r'[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', email_xpath_match_again[-1]):
+                email_list.add('%s@%s' % (email_xpath_match_again[-1], email_xpath_match_again[-2]))
+        return email_list
+
+    def fetch_text_content(self, response):
+        try:
+            text_content = response.xpath(self.TEXT_XPATH).extract()
+            text_content = list(map(lambda y: ' '.join(y), list(map(lambda x: x.split(), text_content))))
+        except IndexError:
+            text_content = 'Nan'
+        return text_content
 
     def parse(self, response):
         # Determine whether the url is valid by calling the instance method determine_validity
@@ -72,32 +160,23 @@ class SchoolWebsiteContentSpider(scrapy.Spider):
         # self.logger.info('Visiting %s' % response.url)
 
         # Get basic information of the page
-        # Get school name
+        # Get school name and page title
         top_domain = get_tld(response.url)
         try:
             school_name = self.inverse_lookup[top_domain]
         except KeyError:
             school_name = 'Unknown School'
-
-        # Get page title
-        try:
-            page_title = response.xpath(self.TITLE_XPATH).extract_first()
-        except NotSupported:
-            return
+        page_title = response.xpath(self.TITLE_XPATH).extract_first()
 
         # Get page text content
-        try:
-            text_content = response.xpath(self.TEXT_XPATH).extract()
-            text_content = list(map(lambda y: ' '.join(y), list(map(lambda x: x.split(), text_content))))
-        except IndexError:
-            text_content = 'Nan'
+        text_content = self.fetch_text_content(response)
         aggregated_text_content = ' <SEPARATOR> '.join(text_content)
 
         # Get email list
-        email_match = re.findall(self.EMAIL_REGEX, str(response.body))
-        email_list = list(set(email_match))
+        email_list = self.fetch_email_list(response)
+
         # Create an item
-        if (len(email_list) >= 1) & (len(email_list) <= 3):
+        if self.determine_personal_profile_component(email=email_list, text=text_content):
             email_dict = dict(zip(list(map(lambda x: 'Email %d' % x, range(len(email_list)))), email_list))
 
             # Get pdf link for possible personal CV
@@ -125,7 +204,7 @@ class SchoolWebsiteListProcessor(object):
         assert(type(url_list) is list)
         self.url_list = url_list
 
-        # Adding corresponding top domains
+        # Add corresponding top domains
         self.url_top_domain = set()
         for url in url_list:
             self.url_top_domain.add(get_tld(url))
