9e47a8eab179df32c382524bf8572a5770007b4f
Uyen <myuyen.le@gmail.com>
2017-10-31 11:29:03 +0800

    dummy post request for login button

--- Integration/group7/application.py
+++ Integration/group7/application.py
@@ -1,30 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-@app.route("/")
+##### Main homepage (not signed in)
+@app.route('/')
 def main():
-    return render_template("main.html")
+    return render_template('main.html')
 
+##### About page
 @app.route("/about/")
 def about():
     return render_template("about.html")
 
+##### Main login page
+@app.route('/login/', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        return redirect(url_for('main_loggedin'))
+
+##### Main homepage (signed in)
 @app.route("/main_loggedin/")
 def main_loggedin():
-	return render_template("main_loggedin.html")
+    return render_template('main_loggedin.html')
 
+##### About page (signed in)
+@app.route('about_loggedin')
+def about_loggedin():
+    return render_template('about_loggedin.html')
+
+##### Profile page
 @app.route("/profile/")
 def profile():
-	return render_template("profile.html")
+    return render_template("profile.html")
 
+##### Crawler page
 @app.route("/crawler/")
 def crawler():
-	return render_template("crawler.html")
+    return render_template("crawler.html")
 
+##### Database page
 @app.route("/database/")
 def database():
-	return render_template("database.html")
+    return render_template("database.html")
 
+##### Initialize app
 if __name__ == "__main__":
     app.run()
     
