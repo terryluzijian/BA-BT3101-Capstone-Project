e4fb93f27ec5d0eecaa415139ac39be3729a7944
Uyen <myuyen.le@gmail.com>
2017-11-02 17:41:36 +0800

    correct university names

--- Integration/group7/helper.py
+++ Integration/group7/helper.py
@@ -3,23 +3,23 @@ def get_peer_unis(dep):
         'Columbia University',
         'Northwestern University',
         'Rice University',
-        'University of California - Los Angeles',
+        'University of California, Los Angeles (UCLA)',
         'University of Michigan',
         'University of Toronto']
         
     biochem_peer = [
         'University of Chicago',
         'The University of Hong Kong',
-        'New York University (NYU)',
+        'New York University',
         'Monash University'
     ]
 
     geo_peer = [
-        'University College London',
+        'UCL (University College London)',
         'University of Colorado - Boulder',
-        'University of Manchester',
+        'The University of Manchester',
         'University of Toronto (St George)',
-        'Queen Mary London']
+        'Queen Mary University of London']
 
     departments = {
         'bme': bme_peer,
@@ -32,7 +32,7 @@ def get_asp_unis(dep):
     bme_asp = [
         'Johns Hopkins University',
         'Georgia Institute of Technology',
-        'University of California-San Diego',
+        'University of California, San Diego (UCSD)',
         'Duke University',
         'Massachusetts Institute of Technology',
         'Stanford University']
@@ -45,9 +45,9 @@ def get_asp_unis(dep):
     ]
      
     geo_asp = [
-        'Cambridge',
+        'University of Cambridge',
         'University of British Columbia',
-        'Oxford']
+        'University of Oxford']
 
     departments = {
         'bme': bme_asp,
