8afd2b294a7662030fe3b065f2bc15dd672e7f8b
Terry Lu Zijian <terryluzijian@gmail.com>
2017-11-03 19:07:19 +0800

    Changed Data File Path

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     name         = 'crawler',
     version      = '1.0',
     packages     = find_packages(),
-    package_data = {'crawler': ['crawler/crawler/data/*.csv', 'crawler/crawler/data/*.json']},
+    package_data = {'crawler': ['data/*.csv', 'data/*.json']},
     entry_points = {'scrapy': ['settings = crawler.settings']},
     zip_safe = False,
 )
+
