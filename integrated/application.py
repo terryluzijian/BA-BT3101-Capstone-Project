cff75a2906c7e15d184d0e47a2885d363f55db9e
Terry Lu Zijian <terryluzijian@gmail.com>
2017-11-04 22:05:38 +0800

    File Name Change

--- integrated/application.py
+++ integrated/application.py
@@ -151,7 +151,7 @@ def crawler_export():
         result = query_db('select name, department, university, tag, position, phd_year, phd_school, promotion_year, text_raw as research_area from profiles where department = ?', (dep_name,))
         result_list = [list(row) for row in result]
         df = pd.DataFrame.from_records(result_list, columns = result[0].keys())
-        df.to_excel('../Results/%s %s.xlsx' % (dep_name, datetime.datetime.now().strftime('%Y-%m-%d')), index=False)
+        df.to_excel('../results/%s %s.xlsx' % (dep_name, datetime.datetime.now().strftime('%Y-%m-%d')), index=False)
         insert_db('insert into activities (activity_timestamp, user_id, activity_name, remark) values (?, ?, ?, ?)',
             (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), session['user_id'], 'export database', helper.get_full_name(dep)))
         return redirect(url_for('crawler', dep=dep))
